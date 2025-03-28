64c65e03e9c4759d7b04ba972606d78ea4b4ce4d
RunVox <robertbsnmolnar@gmail.com>
2025-03-28 23:47:13 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <style>
+
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .loginform {
+            background-color: #fff;
+            border-radius: 8px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            width: 300px;
+            text-align: center;
+        }
+
+        label {
+            font-size: 14px;
+            color: #333;
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            box-sizing: border-box; 
+        }
+
+        input:focus {
+            outline: none;
+            border-color: orange;
+        }
+
+        .loginbutton {
+            width: 100%;
+            padding: 10px;
+            background-color: orange; 
+            color: white;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .loginbutton:hover {
+            background-color: rgb(255, 146, 44); 
+        }
+
+
+        .submitlogin {
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="loginform">
+        <form id="loginForm" action="loginhandler.php" method="post" onsubmit="return redirectToHome()">
+            <label for="email"><b>E-Mail</b></label>
+            <input required id="email" type="text" name="email" placeholder="E-mail.."><br><br>
+
+            <label for="password"><b>Password</b></label>
+            <input required id="password" type="password" name="password" placeholder="Your Password.."><br><br>
+            
+            <div class="submitlogin">
+                <button type="submit" class="loginbutton" id="loginbutton">LOGIN</button>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        function redirectToHome() {
+            event.preventDefault();
+            
+            window.location.href = "index.html";
+        }
+    </script>
+</body
---
+++ register.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTER</title>
+    <style>
+
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .registerform {
+            background-color: #fff;
+            border-radius: 8px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            width: 300px;
+            text-align: center;
+        }
+
+        label {
+            font-size: 14px;
+            color: #333;
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin: 10px 0;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            font-size: 16px;
+            box-sizing: border-box; 
+        }
+
+        input:focus {
+            outline: none;
+            border-color: orange;
+        }
+
+        .registerbutton {
+            width: 100%;
+            padding: 10px;
+            background-color: orange; 
+            color: white;
+            border: none;
+            border-radius: 4px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .registerbutton:hover {
+            background-color:rgb(255, 146, 44);
+        }
+
+        .submitregister {
+            margin-top: 20px;
+        }
+
+        .error-message {
+            color: red;
+            font-size: 12px;
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="registerform">
+        <form id="registerForm" action="registerhandler.php" method="post" onsubmit="return validatePassword()">
+            <label for="name"><b>FULL NAME</b></label>
+            <input required id="name" type="text" name="name" placeholder="YOUR FULL NAME.."><br><br>
+
+            <label for="email"><b>E-Mail</b></label>
+            <input required id="email" type="email" name="email" placeholder="E-MAIL.."><br><br>
+
+            <label for="password"><b>PASSWORD</b></label>
+            <input required id="password" type="password" name="password" placeholder="YOUR PASSWORD.."><br><br>
+
+            <label for="confirm-password"><b>REPEAT YOUR PASSWORD</b></label>
+            <input required id="confirm-password" type="password" name="confirm-password" placeholder="REPEAT YOUR PASSWORD.."><br><br>
+
+            <div class="error-message" id="passwordError">
+                Your password must contain at least a number, a capital and a special character.
+            </div>
+            
+            <div class="submitregister">
+                <button class="registerbutton" id="registerbutton">PRESS TO REGISTER</button>
+            </div>
+        </form>
+    </div>
+
+    <script>
+        function validatePassword() {
+
+            const password = document.getElementById("password").value;
+            const confirmPassword = document.getElementById("confirm-password").value;
+
+            const passwordPattern = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
+
+            // Validăm parola
+            if (!passwordPattern.test(password)) {
+                document.getElementById("passwordError").style.display = "block";
+                return false;
+            }
+
+            if (password !== confirmPassword) {
+                alert("Parolele nu se potrivesc!");
+                return false;
+            }
+
+            return true;
+        }
+    </script>
+</body>
+</html>
